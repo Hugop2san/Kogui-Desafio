969e5dd090305e12f68acf212773d07220207033
Hugop2san <hugopereira2santos60@gmail.com>
2025-07-23 20:35:18 -0300

    finalizacao do arquivo models com usuario e operacao , makemigrations e inicio das configuracoes do serializers...

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    IDusuario   = models.AutoField(primary_key=True) 
+    nome        = models.CharField(max_length=300)
+    Email       = models.CharField(max_length=300)
+    Senha       = models.CharField(max_length=300)
+    Stlnclusao  = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nome
+
+
+
+class Operacao(models.Model):
+    IDOperacao      = models.IntegerField(primary_key=True)
+    IDUsiario       = models.ForeignKey(Usuario, on_delete=models.CASCADE) 
+    Parametros      = models.CharField(max_length=2)
+    Resultado       = models.CharField(max_length=1000)
+    Stlnclusao      = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f" {self.IDUsiario.nome} - {self.Parametros} - {self.Resultado}"
+
+
---
+++ app/serializers.py
@@ -0,0 +1,5 @@
+from .models import Usuario, Operacao
+from rest_framework import serializers
+
+
+# FINALIZAR O SERIALIZER
